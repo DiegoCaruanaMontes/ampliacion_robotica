e531563e186db09ddf1acdf8e6773dd6e18f77fe
Usuario_UMA <Usuario_UMA@172.17.49.134>
2025-03-05 19:53:27 +0100

    P2

---
+++ P2/EJ1.m
@@ -0,0 +1,15 @@
+% Modelo P1
+
+v = 1.2
+dmin = 20 % Separación mínima entre puntos
+T = 0.3
+epsilon = 1
+
+% Probar varios valores de ganancia (Controlador P)
+K = 1
+% Mostrar gráficamente el camino (Coordenadas en metros)
+%figure
+%plot()
+% Gráfica velocidad
+%figure
+%plot()
